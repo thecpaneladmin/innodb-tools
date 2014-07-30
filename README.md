6768d211630acfb98c3eead308e6d70d6abea5a5
Vanessa Vasile <vanessav@inmotionhosting.com>
2014-07-30 17:44:23 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,223 @@
-innodb-tools
-============
+# InnoDB Exporter/Importer
 
-Tools for working with InnoDB
+Python script for exporting and importing InnoDB tables.
+
+Vendor Websites: 
+
+* The cPanel Admin/TCA Server Solutions, LLC <http://thecpaneladmin.com>
+* InMotion Hosting, Inc <http://inmotionhosting.com>
+
+Author: Vanessa Vasile <vanessa@thecpaneladmin.com>
+
+This script does a mass export and import of InnoDB tables for MySQL.  Doing 
+this can resolve a number of InnoDB-related MySQL issues, including:
+
+* InnoDB log sequence mismatches
+* Converting to innodb_file_per_table, and shrinking the shared ibdata1 file
+* Restoring from InnoDB recovery mode where MySQL/InnoDB will not start without
+  recovery being enabled
+
+Usage Scenario Example:
+
+* http://www.thecpaneladmin.com/how-to-convert-innodb-to-innodb_file_per_table-and-shrink-ibdata1/
+
+DISCLAIMER:
+
+As usual, before you make any drastic changes to MySQL data, you should make a
+backup. We are not responsible for any damage done to your system resulting
+from failure to back up data or follow instructions. While this script has been
+repeatedly tested and used in production environments, we must reiterate that
+you use this script at your own risk.
+
+
+## PREREQUISITES
+
+### A.  Running MySQL instance
+
+MySQL must be running for this script to be able to export tables properly.
+If You are having trouble starting MySQL, check the MySQL error log for 
+anything obvious.  If the issue is related to corrupted InnoDB data:
+
+1) Open /etc/my.cnf in a text editor
+2) Add the following line:
+
+    innodb_force_recovery = 1
+
+3) Attempt to start MySQL
+
+If MySQL still does not start, keep increasing the recovery level up to 6
+until it does. If MySQL still refuses to start, your issues are likely 
+outside the scope of what this script would be able to do for you, and you
+should consult a professional.
+
+More information:
+http://dev.mysql.com/doc/refman/5.6/en/forcing-innodb-recovery.html
+
+If you recently upgraded from 5.5 to 5.6, also try downgrading back to 5.5,
+using this script to re-import all tables, then upgrading again.
+
+Note: A lot of InnoDB problems can be resolved simply by starting InnoDB in
+recovery mode (1-4), restarting MySQL, then restarting again without
+recovery mode.
+
+
+### B.  Backups
+
+Make sure you have a backup of your MySQL data.  Since you may be dealing with 
+corrupted data, we'd recommend stopping MySQL and making a copy of your MySQL
+data directory (i.e. /var/lib/mysql).  Again, MySQL should be stopped when you
+do this.  This allows you to easily restore MySQL to the a previous state if
+needed.
+
+If you prefer to use dumps for backups, you can use mysqldump:
+
+http://webcheatsheet.com/sql/mysql_backup_restore.php
+
+
+### C.  Miscellaneous
+
+The following is also needed:
+
+* MySQL 5.0 or higher with client packages installed
+* Python 2.6 or 2.7 with standard modules includes, plus MySQLDB
+* Enough free disk space on the partition this script will dump to
+  (typically the size of your MySQL data folder)
+* Root MySQL access
+* Write permissions to the folder this script is dumping data to
+
+Most standard systems already meet the above requirements.  This script also
+works with MySQL running on cPanel servers.
+
+
+## USAGE
+
+### Exporting tables
+
+The script will only export InnoDB tables, and does so by iterating through
+all databases and dumping only the ones listed as having InnoDB as their
+engine.
+
+Export syntax:
+
+    ./innodb_import_export.py --export [--dir=DIR] [--config=CONFIG]
+
+Where:
+
+    --dir = The folder where you would like the dumps to be stored.  By default
+        this is /home/innodb_data.  A child folder will becreated within this
+        directory for each session, denoted in these examples as $SESSION
+    --config = The client configuration file for MySQL.  By default, this is
+        /root/.my.cnf, but you can create your own
+
+More information:
+http://dev.mysql.com/doc/refman/5.6/en/option-files.html
+
+Export format:
+
+    /$DIR/$SESSION/$DATABASE_NAME/$TABLE_NAME.sql
+
+Log file:
+
+    /$DIR/$SESSION/innodb_export.log
+
+
+### Importing tables
+
+Before you import, you may need to do the following:
+
+* If innodb_recovery_mode > 0, disable it and restart MySQL
+* If you had corruption or are switching to innodb_file_per_table, move the
+  ibdata1 and ib_logfile* files out of the MySQL data folder and restart MySQL.
+  MySQL will automatically recreate these files at default sizes
+
+When you exported, the script provided the name of the export location:
+
+    /$DIR/$SESSION
+
+ie: /home/innodb_data/201407281740/
+
+This is the location we are restoring from.
+
+Import syntax:
+
+    ./innodb_import_export.py --import --dir=DIR [--skip-working] [--config=CONFIG]
+
+Where:
+
+    --dir = The directory containing the exports taken, as discussed above
+    --skip-working = Use this option if you do not want to import tables that
+      are working.  This does not check data integrity, but rather whether
+      MySQL recognizes that the table exists.  You would want to use this
+      option when resuming from a previous restoration
+    --config = The client configuration file for MySQL.  By default, this is
+      /root/.my.cnf, but you can create your own
+
+More information:
+
+http://dev.mysql.com/doc/refman/5.6/en/option-files.html
+
+
+#### Notes:
+
+* When restoring, the .ibd files for each table are moved into the respective
+  table's export folder.  This is to clear up tablespace for corrupted tables
+  that would otherwise prevent an import from occurring.
+
+* If you want to STOP an import, you should avoid using CTRL+C. Instead, touch
+  a stop file in the import folder and the script will stop importing after the
+  current table is done.
+
+  Example:
+
+    touch /$DIR/$SESSION/stop
+
+  Make sure to remove this file before resuming, and consider using
+  --skip-working as to prevent re-importing tables that were already
+  imported.
+
+* If you deleted the ibdata1 and ib_logfile* files, it may be necessary to also
+  remove the .frm files for each table, since ibdata1 will no longer reference
+  them.  You'll know this is necessary if you are not able to import due to
+  MySQL claiming the table could not be dropped or created because it already
+  exists. A quick way to do this is to dump the below lines into a file and run:
+
+  Note: You should stop MySQL before doing this.
+
+```
+    #!/bin/bash
+    DIR='/home/innodb_data/$SESSION/' # PUT THE DIR NAME HERE, ie /$DIR/$SESSION/
+    for file in $(find /var/lib/mysql -name *.frm)
+    do
+        DB=$(dirname $file | xargs basename)
+        TABLE=$(basename $file .frm)
+        /bin/mv -f /var/lib/mysql/$DB/$TABLE.frm $DIR/$DB/
+    done
+```
+
+  This is not coded into the script as doing this may be irreversible and
+  dangerous.  Again, make sure you have a backup.
+   
+   
+### Checking tables
+
+This script can automate post-import sanity checks against InnoDB tables
+
+Check syntax:
+
+    ./innodb_import_export.py --verify
+
+A summary and location of the log file will be given when the script run is
+completed.  The log file will log failures at an ERROR level for review.
+
+One thing to keep in mind here is that this script (and rather any script of
+of this nature, is incapable of verifying the integrity of data within the
+table itself. This script specifically checks that MySQL can access the 
+table, even if it means the table contains partial or no data.  If MySQL can at
+least access the table, it means that 1) data can be imported, and 2) The
+InnoDB engine will not be halted on startup due to the table being corrupted.
+
+If you were running innodb_file_per_table prior to running this script and
+notice that the .ibd files are smaller than they were before, this does not
+mean you lost data, it may mean that unused space within the tablespace file
+was removed, resulting in a smaller file.
+
---
+++ innodb_export_import.py
@@ -0,0 +1,593 @@
+#!/usr/bin/env python
+"""InnoDB Import/Export Script"""
+
+from glob import glob
+import logging
+import os
+import subprocess
+import sys
+from datetime import datetime
+from optparse import OptionParser
+
+try:
+    import MySQLdb
+except ImportError:
+    print 'MySQLdb module not found. To install, run: easy_install MySQL-python'
+    sys.exit(1)
+
+
+def init_logging(log_file):
+    """Initializes logger"""
+
+    inc = datetime.now().strftime('%M%S')
+
+    if os.path.exists(log_file):
+        rename_to = os.path.join(log_file, inc)
+        print 'Moving existing log file to %s' % rename_to
+
+        try:
+            os.rename(log_file, rename_to)
+        except OSError:
+            pass
+
+    open(log_file, 'w').close()
+    logging.basicConfig(format='%(levelname)s: %(message)s',
+                        filename=log_file, level=logging.INFO)
+
+    return True
+
+
+def stop(data_dir):
+    """Checks for a stop file and exits gracefully"""
+
+    # Check for a stop file
+    if os.path.exists(os.path.join(data_dir, 'stop')):
+        msg = 'Stop file detected...stopping'
+        print color_me(msg, 'red')
+        logging.info(msg)
+        return True
+
+    return False
+
+
+def color_me(string, color):
+    """Makes pretty colors if you want 'em"""
+
+    options = opts()[0]
+
+    if not options.do_color:
+        return string
+
+    pretty_colors = {
+        'red': '\033[91m',
+        'orange': '\033[93m',
+        'green': '\033[92m',
+        'blue': '\033[36m',
+        'normal': '\033[0m'
+    }
+
+    return '%s %s %s' % (pretty_colors[color],
+                         string, pretty_colors['normal'])
+
+
+def display_stats(stats):
+    """Displays summary of what this script actually did"""
+
+    summary = {}
+
+    print '\nSUMMARY:\n'
+    for key, value in stats.items():
+        name = key.replace('_', ' ').title()
+        summary[key] = value
+        print '%s: %s' % (name, value)
+
+    return summary
+
+
+def mysql_connect(config, dbname=None):
+    """Establishes a MySQL connection"""
+
+    try:
+        conn = MySQLdb.connect(
+            db=dbname,
+            read_default_file=config
+            )
+
+        dbconn = conn.cursor()
+
+    except MySQLdb.Error, err:
+        print 'MySQL Error %d: %s' % (err.args[0], err.args[1])
+        return None
+
+    return dbconn
+
+
+def get_recovery_level(config):
+    """Checks InnoDB recovery level"""
+
+    dbname = ''
+    dbconn = mysql_connect(config, dbname)
+
+    if not dbconn:
+        return False
+
+    try:
+        dbconn.execute("SHOW VARIABLES LIKE 'innodb_force_recovery'")
+    except (MySQLdb.Error, TypeError), err:
+        print 'MySQL Error %d: %s' % (err.args[0], err.args[1])
+        sys.exit(1)
+
+    result = dbconn.fetchone()[1]
+
+    return result
+
+
+def get_mysql_version(config):
+    """Gets MySQL Server Version"""
+
+    dbname = ''
+    dbconn = mysql_connect(config, dbname)
+
+    if not dbconn:
+        return False
+
+    try:
+        dbconn.execute("SHOW VARIABLES LIKE 'version'")
+    except (MySQLdb.Error, TypeError), err:
+        print 'MySQL Error %d: %s' % (err.args[0], err.args[1])
+        sys.exit(1)
+
+    result = ('').join(dbconn.fetchone()[1].split('-')[0].split('.')[:2])
+
+    return int(result)
+
+
+def database_exists(config, dbname):
+    """Checks if a database exists"""
+
+    dbconn = mysql_connect(config, dbname)
+
+    if not dbconn:
+        return False
+
+    print 'Checking %s...' % dbname
+    try:
+        dbconn.execute("SHOW TABLES")
+    except (MySQLdb.Error, TypeError):
+        return False
+
+    return True
+
+
+def check_table(config, dbname, table):
+    """Checks an InnoDB table"""
+    # Really what we're doing is making sure the tablespace is intact.
+    # This is usually as simply as trying to access the table
+
+    dbconn = mysql_connect(config, dbname)
+
+    if not dbconn:
+        return False
+
+    print 'Checking %s.%s...' % (dbname, table)
+    try:
+        dbconn.execute("EXPLAIN %s" % (table,))
+    except (MySQLdb.Error, TypeError), err:
+        print err
+        return False
+
+    return True
+
+
+def get_dbs_with_innodb(config):
+    """Gets a list of databases that contain InnoDB tables"""
+
+    dbname = 'INFORMATION_SCHEMA'
+    dbconn = mysql_connect(config, dbname)
+
+    if not dbconn:
+        return False
+
+    data = {}
+
+    # This is a little 'eh'.  You can poll for all InnoDB tables via
+    # INFORMATION_SCHEMA, but on very large servers this query can time
+    # out or hang the server. Therefore, we're going to pull a list of
+    # databases and query INFORMATION_SCHEMA individually for all InnoDB
+    # tables within each database.
+
+    # Format: dict = { 'db': ['table_1', 'table_2' ... ]
+
+    print 'Getting a list of databases...'
+    try:
+        dbconn.execute("SHOW DATABASES")
+    except (MySQLdb.Error, TypeError), err:
+        print 'MySQL Error %d: %s' % (err.args[0], err.args[1])
+        return False
+
+    # CHECK THIS - make sure it does not dump mysql
+    databases = [
+        d[0] for d in dbconn.fetchall() if d not in (
+            'mysql', 'information_schema'
+            )
+    ]
+
+    print 'Checking for InnoDB tables...'
+
+    for database in databases:
+
+        print('Database %s...' % database),
+        try:
+            dbconn.execute(
+                "SELECT table_name FROM INFORMATION_SCHEMA.TABLES"
+                " WHERE table_schema='%s' and engine='innodb';" % database
+            )
+        except (MySQLdb.Error, TypeError), err:
+            print 'MySQL Error %d: %s' % (err.args[0], err.args[1])
+            continue
+
+        tables_raw = dbconn.fetchall()
+        if tables_raw:
+            tables = [item[0] for item in tables_raw]
+
+            print 'Detected %s InnoDB tables' % len(tables_raw)
+            data[database] = tables
+        else:
+            print 'No InnoDB tables'
+
+    return data
+
+
+def dump_table(dbname, table, mysql_version, data_dir, config):
+    """Dumps a table"""
+
+    dump_opts = ''
+    if mysql_version >= 56:
+        dump_opts = '--add-drop-trigger' # Only supported in mysqldump for 5.6
+
+    dump_path = os.path.join(data_dir, dbname)
+    if not os.path.exists(dump_path):
+        os.makedirs(dump_path)
+
+    dump_file = os.path.join(dump_path, '%s.sql' % table)
+    fhandle = open(dump_file, 'w+')
+
+    try:
+        process = subprocess.Popen(
+            "mysqldump --defaults-extra-file=%s --add-drop-table %s %s %s" % (
+                config, dump_opts, dbname, table
+            ),
+            stdout=fhandle, stderr=subprocess.PIPE, shell=True)
+        fhandle.close()
+        output = process.stderr.read()
+
+        if output == '':
+            return True
+        else:
+            return False
+
+    except subprocess.CalledProcessError:
+        return False
+
+
+def import_table(dbname, table, data_dir, config):
+    """Imports a table"""
+
+    dump_file = os.path.join(data_dir, dbname, '%s.sql'  % table)
+
+    try:
+        process = subprocess.Popen(
+            "mysql --defaults-file=%s %s < %s" % (
+                config, dbname, dump_file
+
+            ),
+            stderr=subprocess.PIPE, shell=True)
+        output = process.stderr.read()
+
+        if output == '':
+            return True
+        else:
+            return False
+
+    except subprocess.CalledProcessError:
+        return False
+
+
+def main():
+    """Default function"""
+
+    options = opts()[0]
+    timestamp = datetime.now().strftime('%Y%m%d%H%M')
+    config = options.config
+
+    if options.do_export and options.do_import:
+        print 'Please specify either --import or --export'
+        sys.exit(1)
+    elif (not options.do_export
+          and not options.do_import
+          and not options.do_verify):
+        print 'Please specify either --import, --export, or --verify'
+        sys.exit(1)
+
+    mysql_version = get_mysql_version(config)
+    if not mysql_version:
+        print 'Unable to determine MySQL version'
+        sys.exit(1)
+    elif mysql_version < 50:
+        print 'This script requires MySQL 5.0 or higher'
+        sys.exit(1)
+
+    if options.do_export:
+        do_export(options, config, timestamp, mysql_version)
+
+    if options.do_import:
+        do_import(options, config)
+
+    if options.do_verify:
+        do_verify(options, config, timestamp)
+
+
+def do_export(options, config, timestamp, mysql_version):
+    """Handles table exports"""
+
+    # We only want a default dir for exports
+    if not options.data_dir:
+        options.data_dir = '/home/innodb_data'
+
+    data_dir = os.path.join(options.data_dir, timestamp) # Change dir
+    if not os.path.exists(data_dir):
+        os.makedirs(data_dir)
+
+    log_file = os.path.join(data_dir, 'innodb_export.log')
+
+    init_logging(log_file)
+
+    databases = get_dbs_with_innodb(config)
+
+    stats = {
+        'databases_total': len(databases),
+        'tables_total': 0,
+        'tables_exported': 0,
+        'tables_failed': 0,
+    }
+
+    if databases:
+        print 'Dumping tables...'
+    else:
+        print 'No databases detected, or none contain InnoDB data.'
+
+    for dbname, tables in databases.items():
+        for table in tables:
+            stats['tables_total'] += 1
+
+            if stop(data_dir):
+                sys.exit(0)
+
+            if dump_table(dbname, table, mysql_version, data_dir, config):
+                msg = 'Dumped table %s.%s' % (dbname, table)
+                print color_me(msg, 'green')
+                logging.info(msg)
+                stats['tables_exported'] += 1
+            else:
+                msg = 'Error dumping table %s.%s' % (dbname, table)
+                print color_me(msg, 'red')
+                logging.error(msg)
+                stats['tables_failed'] += 1
+
+    summary = display_stats(stats)
+    logging.info(summary)
+
+    print "\nLog file: %s " % log_file
+    print "Tables were dumped to: %s" % data_dir
+
+
+def do_import(options, config):
+    """Handles table imports"""
+
+    # We can't do this if we're in recovery mode
+    if int(get_recovery_level(config)) > 0:
+        print('MySQL is running with innodb_recovery_mode > 0. '
+              'Please disable before continuing.')
+        sys.exit(1)
+    if options.data_dir:
+        data_dir = options.data_dir.rstrip("/")
+    else:
+        print('An import requires a directory containing InnoDB dumps. '
+              ' Please pass --dir with a valid directory name')
+        sys.exit(1)
+
+    if not os.path.exists(data_dir):
+        print 'Data directory "%s" does not exist' % data_dir
+        sys.exit(1)
+
+    log_file = os.path.join(data_dir, 'innodb_import.log')
+    init_logging(log_file)
+
+    if not os.path.exists(data_dir):
+        print 'Import directory %s does not exist' % data_dir
+        sys.exit(1)
+
+    # Loop through the import folder
+    try:
+        databases = os.walk(data_dir).next()[1]
+    except StopIteration:
+        print 'No databases to restore'
+        sys.exit(0)
+
+    stats = {
+        'databases_total': len(databases),
+        'tables_total': 0,
+        'tables_imported': 0,
+        'tables_failed': 0,
+        'tables_skipped': 0
+    }
+
+    for dbname in databases:
+        # Make sure this is a valid database
+        if database_exists(config, dbname):
+
+            # Get tables
+            table_files = glob(os.path.join(data_dir, dbname, '*.sql'))
+            for dump in table_files:
+                table = os.path.splitext(os.path.basename(dump))[0]
+
+                if stop(data_dir):
+                    summary = display_stats(stats)
+                    logging.info(summary)
+                    sys.exit(0)
+
+                stats['tables_total'] += 1
+
+                if options.skip_working:
+                    # Skip tables that are already working, if we said to
+                    if check_table(config, dbname, table):
+                        msg = 'Skipping table %s.%s' % (dbname, table)
+                        print color_me(msg, 'blue')
+                        logging.info(msg)
+                        stats['tables_skipped'] += 1
+
+                # Import the table
+                else:
+                    original = os.path.join(
+                        '/var/lib/mysql/', dbname, '%s.ibd' % table
+                    )
+                    new = os.path.join(data_dir, dbname, '%s.ibd' % table)
+                    print original
+                    try:
+                        os.rename(original, new)
+                        print color_me('Backed up %s' % original, 'blue')
+
+                    except OSError:
+                        pass
+
+                    # Import the table
+                    if import_table(dbname, table, data_dir, config):
+                        msg = 'Imported table %s.%s' % (dbname, table)
+                        print color_me(msg, 'green')
+                        logging.info(msg)
+                        stats['tables_imported'] += 1
+                    else:
+                        msg = 'Error importing table %s.%s' % (dbname, table)
+                        print color_me(msg, 'red')
+                        logging.error(msg)
+                        stats['tables_failed'] += 1
+
+
+    summary = display_stats(stats)
+    logging.info(summary)
+
+    print "\nLog file: %s " % log_file
+
+
+def do_verify(options, config, timestamp):
+    """Handles table sanity checks"""
+
+    # We only want a default dir for exports
+    if not options.data_dir:
+        options.data_dir = '/home/innodb_data'
+
+    data_dir = os.path.join(options.data_dir, timestamp)
+    if not os.path.exists(data_dir):
+        os.makedirs(data_dir)
+
+    log_file = os.path.join(data_dir, 'innodb_check.log')
+
+    init_logging(log_file)
+
+    if not os.path.exists(data_dir):
+        print 'Import directory %s does not exist' % data_dir
+        sys.exit(1)
+
+    databases = get_dbs_with_innodb(config)
+
+    stats = {
+        'databases_total': len(databases),
+        'tables_total': 0,
+        'tables_checked': 0,
+        'tables_ok': 0,
+        'tables_bad': 0,
+    }
+
+    if databases:
+        print 'Checking tables...'
+    else:
+        print 'No databases detected!'
+
+    for dbname, tables in databases.items():
+        for table in tables:
+            if stop(data_dir):
+                summary = display_stats(stats)
+                logging.info(summary)
+                sys.exit(0)
+
+            stats['tables_total'] += 1
+            stats['tables_checked'] += 1
+
+            if check_table(config, dbname, table):
+                msg = 'Table %s.%s is OK' % (dbname, table)
+                print color_me(msg, 'green')
+                logging.info(msg)
+                stats['tables_ok'] += 1
+            else:
+                msg = 'Table %s.%s has errors' % (dbname, table)
+                print color_me(msg, 'red')
+                logging.error(msg)
+                stats['tables_bad'] += 1
+
+    summary = display_stats(stats)
+    logging.info(summary)
+
+    print "\nLog file: %s " % log_file
+
+
+def opts():
+    """Defines valid command line options"""
+
+    parser = OptionParser(
+        usage='usage: %prog [options]',
+        version='%prog 1.0'
+    )
+    parser.add_option(
+        '-e', '--export',
+        help='Export InnoDB tables',
+        action='store_true', dest='do_export', default=False
+        )
+    parser.add_option(
+        '-i', '--import',
+        help="Import InnoDB data",
+        action='store_true', dest='do_import', default=False
+        )
+    parser.add_option(
+        '-v', '--verify',
+        help="Verify InnoDB tables",
+        action='store_true', dest='do_verify', default=False
+        )
+    parser.add_option(
+        '-d', '--dir',
+        help="Location of InnoDB data (default: /home/innodb_data)",
+        action='store', type='string', dest='data_dir'
+    )
+    parser.add_option(
+        '-s', '--skip-working',
+        help="Skip import of tables that are a already working",
+        action='store_true', dest='skip_working', default=False
+    )
+    parser.add_option(
+        '-c', '--config',
+        help="Local user config file (default: /root/.my.cnf)",
+        action='store', type='string', dest='config', default='/root/.my.cnf'
+    )
+    parser.add_option(
+        '-p', '--colors',
+        help="Color output; Makes failures more visible.",
+        action='store_true', dest='do_color', default=False
+    )
+
+    (options, args) = parser.parse_args()
+
+    return options, args
+
+
+if __name__ == '__main__':
+    main()
+
